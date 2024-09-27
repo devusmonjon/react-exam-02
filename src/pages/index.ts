412103a4444e2f4d082ed5e89ab6e1971d0f6aca
devusmonjon <usmonjonhasanov777@gmail.com>
2024-09-27 10:53:46 +0500

    empty

--- src/pages/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Home } from "./home/home";
-export { default as Products } from "./prodcuts/products";
-export { default as Cart } from "./cart/cart";
-export { default as SingleProduct } from "./single-product/single-product";
